332ed8e4372eea660910f35b403e5cd5b7736813
unknown <insidepower@.(none)>
2010-07-04 18:54:25 +0800

    hide directional pad, added img_stone_w

--- 100gui.py
+++ 100gui.py
@@ -1,4 +1,5 @@
 import appuifw, e32, graphics
+import sysinfo
 
 #Define the exit function
 def quit():app_lock.signal()
@@ -9,33 +10,28 @@ selectedindex=0
 def handle_redraw(rect):
      canvas.blit(img)
 
-def handle_redraw(rect):
-    global photo
-    canvas.blit(photo)
+def selected_index():
+    return selectedindex
 
-def quit():
-    app_lock.signal()
-
-
+#(width, height) = sysinfo.display_pixels()
 img=graphics.Image.new((360,640))
+#img=graphics.Image.new((width,height))
+#print("size is %d %d" %(width, height))
 
-img2=graphics.Image.open("c:\\Data\\python\\board.jpg")
+img_board=graphics.Image.open("c:\\Data\\python\\board.jpg")
+img_stone_w=graphics.Image.open("c:\\Data\\python\\box.jpg")
 #img2=graphics.Image.open("e:\\Images\\area.png")
 
 #draw list item background
-img.blit(img2, (0,0), (0,0))
+img.blit(img_board, (0,0), (0,0))
 #draw list item text
 img.text((30,30),u'List Text',(0,0,0),"title")
 
+# hide the virtual directional pad
+appuifw.app.directional_pad=False;
 canvas=appuifw.Canvas(event_callback=None, redraw_callback=handle_redraw)
 appuifw.app.body=canvas
-canvas.bind(key_codes.EKeySelect, press_select)
-canvas.bind(key_codes.EKeyDownArrow, press_down)
-canvas.bind(key_codes.EKeyUpArrow, press_up)
-canvas.bind(key_codes.EKeyRightArrow, press_right)
-canvas.bind(key_codes.EKeyLeftArrow, press_left)
 
-appuifw.app.exit_key_handler = quit
 app_lock=e32.Ao_lock()
 app_lock.wait()
 
